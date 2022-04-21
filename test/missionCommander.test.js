065e2fbf58bd69c5aef71e8cd623a27d8fbc0dfb
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-20 22:21:17 -0500

    Create New Test

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,6 +1,15 @@
+const MissionCommander = require("./../app/missionCommander");
+
 describe("Esto es una suite de pruebas", () => {
   test("Caso de prueba", () => {
     const result = 1 + 2;
     expect(result).toBe(3);
+  });
+});
+
+describe("Unit test for Mission Commander Class", () => {
+  test("1.- Create Mission Commander Object", () => {
+    const myMissionCommander = new MissionCommander("Woopa");
+    expect(myMissionCommander.name).toBe("Woopa");
   });
 });
